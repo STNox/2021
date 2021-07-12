475c79709ad8ff0720bb92017f85152e20b5c02d
STNox <caush@naver.com>
2021-07-12 15:50:53 +0900

    exe

--- gui_basic/3_label.py
+++ gui_basic/3_label.py
@@ -1,4 +1,13 @@
+import os
 from tkinter import *
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 root = Tk()
 
@@ -8,7 +17,7 @@ root.geometry('640x480')
 label1 = Label(root, text='안녕하세요')
 label1.pack()
 
-photo = PhotoImage(file='gui_basic/img.png')
+photo = PhotoImage(file=resource_path('gui_basic/img.png'))
 label2 = Label(root, image=photo)
 label2.pack()
 
@@ -16,7 +25,7 @@ def change():
     label1.config(text='또 만나요')
 
     global photo2 # 함수 내에서 레이블의 이미지를 바꾸기 위해 전역 변수 선언
-    photo2 = PhotoImage(file='gui_basic/img2.png')
+    photo2 = PhotoImage(file=resource_path('gui_basic/img2.png'))
     label2.config(image=photo2)
 
 btn = Button(root, text='클릭', command=change)
